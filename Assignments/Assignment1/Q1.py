d9b887672782f880b9c3baba7429ac459b5200af
Muqadsa Qudoos <muqadsaqudoos@gmail.com>
2024-02-04 15:47:14 +0500

    assignment 1

---
+++ Assignments/Assignment1/Q1.py
@@ -0,0 +1,16 @@
+def is_palindrom_by_recursion(string,s,e):
+    if s >= e :
+        return True
+    if string[s] == string[e]:
+        return is_palindrom_by_recursion(string,s+1,e-1)
+    
+    else:
+        return False
+    
+a = "abba"
+b = is_palindrom_by_recursion(a,0,3)
+if b:
+    print(f"{a} is Palindrom")
+
+else:
+    print(f"{a} is not Palindrom")
