8afac1c4706d03d49548a6f765861b5cd73fb3f1
Muqadsa Qudoos <muqadsaqudoos@gmail.com>
2024-03-15 00:08:53 +0500

    selection sort

---
+++ class_work/lec_9/selection_sort.py
@@ -0,0 +1,13 @@
+def selection_sort(arr):
+    for i in range(1,len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -=1
+        arr[j+1] = key
+    return arr
+
+
+a = [0,9,4,3,11,15,2,5,8,3,1,9,100,-1]
+print(selection_sort(a))
